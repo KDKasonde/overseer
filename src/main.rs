f0025228926243dbcdc5fd3e9dfeeb1522e70139
Kasonde Kasonde <d.kasonde99@gmail.com>
2024-01-31 20:54:56 +0000

    feat: add methods to retrieve account cash

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = &args.api_key;
 
     let trading_212_base_api = "https://live.trading212.com/api/v0/".to_string();
-    let trading_212_account = format!("{trading_212_base_api}equity/account/cash");
 
-    let client = overseer::Trading212::new(&trading_212_base_api, &api_key).client;
-    let res = client.get(trading_212_account)
-        .header("Authorization", api_key)
-        .send()
-        .await?
-        .text()
-        .await?;
+    let client = overseer::Trading212::new(&trading_212_base_api, &api_key);
+    let res = client.fetch_account_metadata()
+        .await;
     println!("The response was {}", res);
     Ok(())
     
